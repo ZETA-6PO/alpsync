fc6f9b54cabc8ce7c6deb13c85d2e7fccdb90e02
Hugo BOUVERAT <hugo.bouverat@epita.fr>
2024-05-21 16:20:51 +0800

    fix: tring to fix err again

--- api/routes.go
+++ api/routes.go
@@ -2,12 +2,15 @@ package api
 
 import (
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func InitRoutes(router *mux.Router) {
-
+	// Servir les fichier de facon statique
+	router.PathPrefix("/f/").HandlerFunc(downloadHandler)
+	fs := http.FileServer(http.Dir("./static/"))
+	router.PathPrefix("/").Handler(fs)
 	router.HandleFunc("/uploadOk", uploadOk)
-	router.PathPrefix("/f").Subrouter().HandleFunc("/", downloadHandler)
 	apiRouter := router.PathPrefix("/api").Subrouter()
 	apiRouter.HandleFunc("/status", statusHandler).Methods("GET")
 	apiRouter.HandleFunc("/upload", uploadFileHandler).Methods("POST")
